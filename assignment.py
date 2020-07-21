2c17be7189499cbc3e564413cac8a4b2a3e40963
sandrocky16 <68579518+sandrocky16@users.noreply.github.com>
2020-07-21 20:09:45 +0530

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,19 @@
+a=['1','2','3']
+a.append(['4','5'])
+print("the appended function is : ",a)
+b=['1','2','3']
+b.extend(['4','5'])
+print("the extended function is : ",b)
+c=['1', '2', '3', '4', '5']
+c.insert(3,"8")
+print("the inserted function is : ",c)
+
+
+
+a=('1','2','3','4','5')
+print(a[1])
+
+
+f={'one':1,'two':2}
+del f['one']
+
